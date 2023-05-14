e967b6f4fe05f2908cc0c770df6410afd6e503a8
Alexandre Bastos <xande.bastos321@gmail.com>
2023-05-13 21:27:23 -0300

    concluido layout pagina inicial, adicionado img astronaut

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,11 @@ import Main from "@/components/Main"
 
 export default function Home() {
   return (
-    <div className="h-screen">
+    <>
       <header>
         <Navbar />
         <Main />
       </header>
-    </div>
+    </>
   )
 }
--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,20 +1,32 @@
+import Image from "next/image";
+
 const Main = () => {
   return (
     <main className="flex flex-col px-40 w-screen space-y-10 h-128">
-      <div className="flex flex-col h-128 justify-center">
-        <h1 className="text-5xl font-light">Hi! I&apos;m Alexandre Bastos</h1>
+      <div className="flex flex-row justify-around">
+        <div className="flex flex-col h-128 justify-center">
+          <h1 className="text-5xl font-light">Hi! I&apos;m Alexandre Bastos</h1>
 
-        <p className="text-1xl py-10">
-          Software engineer with experience in the Javascript/TypeScript stack,
-          technology
-          <br /> enthusiast, always seeking to learn new technologies, currently
-          open to work.
-        </p>
+          <p className="text-1xl py-10">
+            Software engineer with experience in the Javascript/TypeScript
+            stack, technology
+            <br /> enthusiast, always seeking to learn new technologies,
+            currently open to work.
+          </p>
 
-        <div className="flex py-14">
-          <a href="" className="hover:text-hovertext">
-            see more about me
-          </a>
+          <div className="flex py-14">
+            <a href="" className="hover:text-hovertext">
+              see more about me
+            </a>
+          </div>
+        </div>
+        <div>
+          <Image
+            src="/astronaut.png"
+            alt="astrounaut"
+            width={650}
+            height={500}
+          />
         </div>
       </div>
     </main>
