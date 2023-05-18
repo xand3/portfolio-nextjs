070c5953bf9867883bb866b94d4fec48728b2b9e
Alexandre Bastos <xande.bastos321@gmail.com>
2023-05-17 23:07:56 -0300

    Atualizando pagina resume

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -6,7 +6,7 @@ export default function ResumePage() {
   return (
     <MainArea>
       <LeftArea>
-        <embed className="max-sm:h-72" src="/resume.pdf" type="application/pdf" width="100%" height="100%" />
+        <embed className="max-sm:h-72" src="/my-resume.pdf" type="application/pdf" width="100%" height="100%" />
       </LeftArea>
       <div>
         <ImgAstronaut width={600}/>
