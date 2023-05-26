e3fd759f3fe9112dd1905129c163734ff8057bbe
Alexandre Bastos <xande.bastos321@gmail.com>
2023-05-26 15:01:04 -0300

    feito layout da pagina de projetos

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -4,24 +4,29 @@ import LeftArea from "../../components/LeftArea";
 import ImgAstronaut from "../../components/Img";
 import Image from "next/image";
 
-
 export default function ProjectsPage() {
   return (
     <MainArea>
-      <LeftArea myClasses = "justify-start">
+      <LeftArea myClasses="justify-start">
         <h1 className="text-5xl font-light max-lg:text-2xl py-5">Projects</h1>
-        <div className="grid grid-cols-2">
-          <div>
-            conteudo 1
+        <div className="grid grid-cols-2 grid-rows-2">
+          <div className="h-8">
+            projeto 1
+          </div>
+          <div className="h-8">
+            projeto 2
           </div>
           <div>
-            conteudo 2
+            projeto 3
+          </div>
+          <div>
+            projeto 4
           </div>
         </div>
       </LeftArea>
       <div>
-        <ImgAstronaut width={600}/>
+        <ImgAstronaut width={600} />
       </div>
     </MainArea>
-  )
+  );
 }
