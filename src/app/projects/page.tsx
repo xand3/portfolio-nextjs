ad405523af4c6f65de7aec2adbdc540b51c68ecd
Alexandre Bastos <xande.dev@outlook.com>
2023-12-17 17:34:45 -0300

    :construction: Atualizando página de projetos

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -14,8 +14,6 @@ export default function ProjectsPage() {
 
         <div className={`grid grid-cols-2 grid-rows-auto gap-y-12 gap-x-8 max-sm:grid-cols-1 max-sm:gap-y-28`}>
 
-          <Project imgSrc={"/images/pokedex.png"} title={"Pokedex"} description={"In this project, I have developed an online Pokédex: a virtual encyclopedia that contains all Pokémon species."} repositoryLink={"https://github.com/xand3/pokedex-dioBootcamp"} projectLink={"https://xand3.github.io/pokedex-dioBootcamp/"} />
-
           <Project imgSrc={"/images/codificador.png"} title={"Codificador"} description={"In this challenge during the Oracle training, I had to develop a text encoder using HTML and CSS to build the project's layout, and JavaScript to implement the encryption logic."} repositoryLink={"https://github.com/xand3/Codificador-Challenge-Oracle"} projectLink={"https://xand3.github.io/Codificador-Challenge-Oracle/"} />
 
           <Project imgSrc={"/images/jogo-da-forca.png"} title={"Jogo da forca"} description={"In this challenge during the Oracle training, I had to develop a Hangman game using HTML and CSS to create the project's layout and JavaScript to implement the game logic."} repositoryLink={"https://github.com/xand3/JogoDaForca-Challenge-Oracle"} projectLink={"https://xand3.github.io/JogoDaForca-Challenge-Oracle/"} />
